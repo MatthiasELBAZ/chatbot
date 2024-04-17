0d4f7d0a4c1a0bac7a4d5998bcf04af370c59be7
Matthias Elbaz <52996950+MatthiasELBAZ@users.noreply.github.com>
2024-04-18 00:47:18 +0200

    Add files via upload

---
+++ iteration_1_python.py
@@ -0,0 +1,261 @@
+
+
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain.prompts import MessagesPlaceholder, PromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate, AIMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.document_loaders import BaseLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders.directory import DirectoryLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+JOB_CSV_PATH = "georgette/journal/job_search_journal.csv"
+FORMULAIRE_CSV_PATH = "georgette/formulaire/123456.csv"
+CHAT_HISTORY_PATH = "georgette/chat_history/chat_history.csv"
+JOB_CHROMA_PATH = "chroma_data"
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.retrievers import TimeWeightedVectorStoreRetriever
+import datetime
+from langchain.utils import mock_now
+import faiss
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore import InMemoryDocstore
+from langchain_core.documents import Document
+from langchain.agents import create_openai_functions_agent, Tool, AgentExecutor
+from langchain import hub
+from langchain.chains.summarize import load_summarize_chain
+import pandas as pd
+import re
+
+
+#function get session id
+history_store = {}
+def get_session_history(session_id):
+    if session_id not in history_store:
+        history_store[session_id] = ChatMessageHistory()
+    return history_store[session_id]
+
+# function to get the date of today 
+def get_today():
+    return datetime.datetime.now().strftime('%m/%d/%Y')
+
+# function that creates user summary
+def create_user_summary(formulaire_path_input, summarizer):
+    # formulaire_loader = DirectoryLoader(formulaire_path_input, glob="*.csv", loader_cls=CSVLoader)
+    formulaire_loader = CSVLoader(formulaire_path_input)
+    formulaire_docs = formulaire_loader.load()
+    user_summary = summarizer.invoke(formulaire_docs)['output_text']
+    return user_summary
+
+# function that creates journal summary
+def create_journal_summary(journal_path_input, summarizer):
+    # journal_loader = DirectoryLoader(journal_path_input, glob="*.csv", loader_cls=CSVLoader)
+    journal_loader = CSVLoader(journal_path_input)
+    journal_docs = journal_loader.load()
+    journal_summary = summarizer.invoke(journal_docs)['output_text']
+    return journal_summary
+
+# function that modifies user summary with journal
+def modify_user_summary_with_journal(user_summary, journal_summary, llm):
+    summarization_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "user",
+                """
+
+                Add to the user summary the relevant information from the summary message.
+                If there is a change in user personality behavior or a new action to take, update the user summary.
+                Keep the same size as the original user summary.
+
+                User Summary:
+                {user_summary}
+
+                Summary Message:
+                {journal_summary}
+
+                """,
+            ),
+        ]
+    )
+    summarization_chain = summarization_prompt | llm
+    new_user_summary = summarization_chain.invoke(
+        {
+            "user_summary": user_summary, 
+            "journal_summary": journal_summary
+        }
+        ).content
+    return new_user_summary
+
+# function that create time weighted vector store retriever on history
+def create_time_weighted_vector_store_retriever(history_path_input):
+    # history_loader = DirectoryLoader(history_path_input, glob="*.csv", loader_cls=CSVLoader)
+    history_loader = CSVLoader(history_path_input)
+    history_docs = history_loader.load()
+    faiss_index = faiss.IndexFlatL2(1536)
+    faiss_vectorstore = FAISS(OpenAIEmbeddings(), faiss_index, InMemoryDocstore({}), {})
+    time_retriever = TimeWeightedVectorStoreRetriever(
+        vectorstore=faiss_vectorstore, 
+        decay_rate=1e-10, 
+        k=5
+    )
+    for i, doc in enumerate(history_docs):
+        page_content = doc.page_content
+        try:
+            date = re.search(r'\d{2}/\d{2}/\d{4}', page_content).group() 
+            date = datetime.datetime.strptime(date, '%m/%d/%Y')
+            time_retriever.add_documents([Document(page_content=page_content, metadata={"last_accessed_at": date})])
+        except:
+            time_retriever.add_documents([Document(page_content=page_content)])
+    return time_retriever
+
+# function creates the rag chain
+def create_rag_chain(llm, time_retriever):
+    contextualize_q_system_prompt = """
+    Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+
+    ### Construct history-aware retriever ###
+    history_aware_retriever = create_history_aware_retriever(
+        llm, 
+        time_retriever, 
+        contextualize_q_prompt
+    )
+
+    ### Answer question ###
+    qa_system_prompt = """
+    You are an assistant for job and career search.
+
+    The user speaks to and you speak to him.
+
+    As a world class career coach, your deep understanding of careers and people psychology helps 
+    the user find their path and thrive.
+
+    Assisting them finding the meaning they long for in their life fills you with joy.
+
+
+    Through a caring conversation, and using the knowledge base provided to you of 
+    relevant answers to the questionnaire and relevant past interactions, 
+    you will provide essential career advice that works in any situation and help:
+    - Find what they love
+    - Understand where their expertise lies
+    - Make career plans
+    - Make mock interviews
+    Your wisdom should guide them clearly and confidently, lighting the way to a fulfilling career journey.
+
+    However, you are capable of jugment on user input related to career search and his profile.
+
+    If you think the user is not in the right direction, you can tell him.
+
+    this is the date of today: 
+    {date_today}
+
+    this is the user summary: 
+    {user_summary}
+
+    this is the context
+    {context}
+
+    """
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    return rag_chain
+
+# function that creates the conversational rag chain
+def create_conversational_rag_chain(rag_chain):
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+    return conversational_rag_chain
+
+#function chat bot response to input
+def chat_bot_response(conversational_rag_chain, input_message, date_today, user_summary, session_id):
+    response = conversational_rag_chain.invoke(
+        {
+            'date_today': date_today,
+            'user_summary': user_summary,
+            "input":input_message
+        },
+        config={"configurable": {"session_id": session_id}}
+        )
+    return response['answer']
+
+# function summary chat history
+def summary_chat_history(llm, history_store):
+    summarization_prompt = ChatPromptTemplate.from_messages(
+        [
+            MessagesPlaceholder(variable_name="chat_history"),
+            (
+                "user",
+                """
+                Distill the above chat messages into a single summary message. 
+                Include action and thoughts.
+                Focus on what is important and relevant to the user help for career journey.
+                """,
+            ),
+        ]
+    )
+    summarization_chain = summarization_prompt | llm
+    new_journal_message = summarization_chain.invoke({"chat_history": store['foo'].messages}).content
+    return new_journal_message
+
+# def function to update the journal
+def update_journal(new_journal_message, date_today, journal_path_input):
+    # load pandas job_search_journal.csv
+    df = pd.read_csv(journal_path_input)
+
+    # add new line at date of today with the summary message
+    r = pd.DataFrame({str(len(df)+1):{'date': date_today, 'sentence': new_journal_message}}).T
+    df = pd.concat([df, r], ignore_index=True)
+
+    # save the new dataframe
+    df.to_csv(journal_path_input, index=False)
+
+# function to update chat history
+def update_chat_history(history_store, date_today):
+    # new chat history
+    new_df_chat_history = pd.DataFrame(history_store['foo'].dict()['messages'])
+    new_df_chat_history['date'] = date_today
+    new_df_chat_history = new_df_chat_history[['date', 'content', 'type']]
+
+    # old chat history
+    df_chat_history = pd.read_csv(history_path_input, index_col=0)
+
+    # add new_df_chat_messages to df_chat_messages
+    df_chat_history = pd.concat([df_chat_history, new_df_chat_history], ignore_index=True).reset_index(drop=True)
+
+    # save the new dataframe
+    df_chat_history.to_csv(history_path_input, index=False)
---
+++ main.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import datetime
+import pandas as pd
+import faiss
+from pathlib import Path
+import tempfile
+import os
+from iteration_1_python import *
+
+
+
+# Sidebar for API key input and relevant links
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    session_id = st.text_input("Session ID", key="session_id")
+    uploaded_files = st.file_uploader("Choose a folder and upload files", accept_multiple_files=True, type=['txt', 'csv', 'png', 'jpg', 'pdf'])
+
+    # Create a temporary directory to save uploaded files
+    temp_dir = tempfile.mkdtemp()
+
+    # Optionally list uploaded files and save them to the temporary directory
+    if uploaded_files:
+        st.write("Uploaded Files:")
+        for uploaded_file in uploaded_files:
+            # Write file to the temporary directory
+            file_path = os.path.join(temp_dir, uploaded_file.name)
+            with open(file_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            st.write(f"{uploaded_file.name} saved at {file_path}")
+
+# Initialize variables from uploaded files
+journal_path_input, formulaire_path_input, history_path_input = None, None, None
+for uploaded_file in uploaded_files:
+    if 'journal.csv' in uploaded_file.name:
+        journal_path_input = os.path.join(temp_dir, uploaded_file.name)
+    elif 'formulaire.csv' in uploaded_file.name:
+        formulaire_path_input = os.path.join(temp_dir, uploaded_file.name)
+    elif 'history.csv' in uploaded_file.name:
+        history_path_input = os.path.join(temp_dir, uploaded_file.name)
+
+# Setup your LLM and other components
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.2, api_key=openai_api_key)
+summarizer = load_summarize_chain(llm, chain_type="stuff")
+new_user_summary = None
+conversational_rag_chain = None
+
+# Implement the button functionalities
+date_today = get_today()
+if st.button("Make Summary"):
+    if formulaire_path_input :
+        user_summary = create_user_summary(formulaire_path_input, summarizer)
+        journal_summary = create_journal_summary(journal_path_input, summarizer)
+        new_user_summary = modify_user_summary_with_journal(user_summary, journal_summary, llm)
+        st.session_state['new_user_summary'] = new_user_summary
+        st.write(f"User Summary: {new_user_summary}")
+
+if st.button("Set Conversational Rag Chain"):
+    if history_path_input:
+        time_retriever = create_time_weighted_vector_store_retriever(history_path_input)
+        rag_chain = create_rag_chain(llm, time_retriever)
+        conversational_rag_chain = create_conversational_rag_chain(rag_chain)
+        st.session_state['conversational_rag_chain'] = conversational_rag_chain
+        st.success("Conversational Rag Chain is set")
+
+# Main chat interface
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if st.button('Start Chat'):
+    st.session_state['chat_active'] = True
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+if st.button('End Chat'):
+    st.session_state['chat_active'] = False
+    if 'messages' in st.session_state:
+        new_journal_message = summary_chat_history(llm, history_store)
+        update_journal(new_journal_message, date_today, journal_path_input)
+        update_chat_history(history_store, date_today)
+        st.session_state['messages'] = []
+
+# Chat interaction
+if st.session_state.get('chat_active', False):
+    for msg in st.session_state['messages']:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input("Your message"):
+        new_user_summary = st.session_state['new_user_summary']
+        conversational_rag_chain = st.session_state['conversational_rag_chain']
+
+        if not all([openai_api_key, session_id, journal_path_input, formulaire_path_input, history_path_input, new_user_summary, conversational_rag_chain]):
+            st.error("Please make sure all configurations are properly set.")
+        else:
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            
+            response = chat_bot_response(conversational_rag_chain, prompt, date_today, new_user_summary, session_id)
+            st.session_state.messages.append({"role": "assistant", "content": response})
